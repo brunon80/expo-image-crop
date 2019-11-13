ab0480f42dd7847203b88f842d6169d26127dc19
Brunon80 <bruno_rocha2008@hotmail.com>
2019-11-13 16:45:20 -0300

    new example UI

--- example/App.js
+++ example/App.js
@@ -1,78 +1,120 @@
 import React from 'react'
 import {
-    Dimensions, Button, ImageBackground, View,
+    Dimensions, View, Image, Text,
 } from 'react-native'
 import * as ImagePicker from 'expo-image-picker'
 import * as Permissions from 'expo-permissions'
+import Icon from 'react-native-vector-icons/MaterialIcons'
 import ImageManipulator from './manipulator/ImageManipulator'
 
+import HybridTouch from './HybridTouch'
+
+const noImage = require('./assets/no_image.png')
+
 export default class App extends React.Component {
-  state = {
-      isVisible: false,
-      uri: 'https://i.pinimg.com/originals/39/42/a1/3942a180299d5b9587c2aa8e09d91ecf.jpg',
-  }
-  onToggleModal = () => {
-      const { isVisible } = this.state
-      this.setState({ isVisible: !isVisible })
-  }
-  _pickImage = async () => {
-      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL)
-      if (status === 'granted') {
-          const result = await ImagePicker.launchImageLibraryAsync()
+    state = {
+        isVisible: false,
+        uri: null,
+    }
+    onToggleModal = () => {
+        const { isVisible } = this.state
+        this.setState({ isVisible: !isVisible })
+    }
+    _pickImage = async () => {
+        const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL)
+        if (status === 'granted') {
+            const result = await ImagePicker.launchImageLibraryAsync()
+            if (!result.cancelled) {
+                this.setState({
+                    uri: result.uri,
+                }, () => this.setState({ isVisible: true }))
+            }
+        }
+    };
 
-          if (!result.cancelled) {
-              this.setState({ uri: result.uri })
-          }
-      }
-  };
+    _pickCameraImage = async () => {
+        const { status } = await Permissions.askAsync(Permissions.CAMERA)
+        if (status === 'granted') {
+            const result = await ImagePicker.launchCameraAsync()
 
-  _pickCameraImage = async () => {
-      const { status } = await Permissions.askAsync(Permissions.CAMERA)
-      if (status === 'granted') {
-          const result = await ImagePicker.launchCameraAsync()
+            if (!result.cancelled) {
+                this.setState({
+                    uri: result.uri,
+                }, () => this.setState({ isVisible: true }))
+            }
+        }
+    };
 
-          if (!result.cancelled) {
-              this.setState({ uri: result.uri })
-          }
-      }
-  };
-
-  render() {
-      const { uri, isVisible } = this.state
-      const { width, height } = Dimensions.get('window')
-      return (
-          <ImageBackground
-              resizeMode="contain"
-              style={{
-                  justifyContent: 'center', padding: 20, alignItems: 'center', height, width, backgroundColor: 'black',
-              }}
-              source={{ uri }}
-          >
-              <Button title="Open Image Editor" onPress={() => this.setState({ isVisible: true })} />
-              <View style={{ margin: 20 }} />
-              <Button title="Get Image from Image Library" onPress={() => this._pickImage()} />
-              <View style={{ margin: 20 }} />
-              <Button title="Take Picture" onPress={() => this._pickCameraImage()} />
-              <ImageManipulator
-                  photo={{ uri }}
-                  isVisible={isVisible}
-                  onPictureChoosed={({ uri: uriM }) => this.setState({ uri: uriM })}
-                  onToggleModal={this.onToggleModal}
-                  saveOptions={{
-                      compress: 1,
-                      format: 'png',
-                      base64: true,
-                  }}
-                  btnTexts={
-                      {
-                          crop: 'Recortar',
-                          rotate: 'Rotacionar',
-                          done: 'Pronto',
-                          processing: 'Processando',
-                      }
-                  }
-              />
-          </ImageBackground>
-      )
-  }
+    render() {
+        const {
+            uri, isVisible,
+        } = this.state
+        const { width, height } = Dimensions.get('window')
+        return (
+            <View style={{
+                backgroundColor: '#fcfcfc',
+                justifyContent: 'center',
+                alignContent: 'center',
+                alignItems: 'center',
+                height,
+            }}
+            >
+                {uri ? (
+                    <Image resizeMode="contain"
+                        style={{
+                            width, height, marginBottom: 40, backgroundColor: '#fcfcfc',
+                        }}
+                        source={{ uri }}
+                    />
+                ) : <Image source={noImage} />}
+                <View style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-around',
+                    backgroundColor: '#2c98fd',
+                    width,
+                    position: 'absolute',
+                    bottom: 0,
+                    padding: 20,
+                }}
+                >
+                    <HybridTouch style={{ flex: 1, alignItems: 'center' }} onPress={() => this._pickImage()}>
+                        <View style={{ alignItems: 'center' }}>
+                            <Icon size={30} name="photo" color="white" />
+                            <Text style={{ color: 'white', fontSize: 18 }}>Galery</Text>
+                        </View>
+                    </HybridTouch>
+                    <HybridTouch style={{ flex: 1, alignItems: 'center' }} onPress={() => this._pickCameraImage()}>
+                        <View style={{ alignItems: 'center' }}>
+                            <Icon size={30} name="photo-camera" color="white" />
+                            <Text style={{ color: 'white', fontSize: 18 }}>Camera</Text>
+                        </View>
+                    </HybridTouch>
+                </View>
+                {
+                    uri
+                && (
+                    <ImageManipulator
+                        photo={{ uri }}
+                        isVisible={isVisible}
+                        onPictureChoosed={({ uri: uriM }) => this.setState({ uri: uriM })}
+                        onToggleModal={this.onToggleModal}
+                        saveOptions={{
+                            compress: 1,
+                            format: 'png',
+                            base64: true,
+                        }}
+                        btnTexts={
+                            {
+                                crop: 'Recortar',
+                                rotate: 'Rotacionar',
+                                done: 'Pronto',
+                                processing: 'Processando',
+                            }
+                        }
+                    />
+                )
+                }
+            </View>
+        )
+    }
 }
