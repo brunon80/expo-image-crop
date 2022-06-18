5a9c7159b61bea8d3924fdbf7561f687c4e75400
Masamoto Miyata <miyata@sincere-co.com>
2022-06-18 19:06:27 +0900

    Install typescript dependencies.

--- example/App.js
+++ example/App.js
@@ -1,125 +1,125 @@
-import React from 'react'
+import React from 'react';
 import {
-    Dimensions, View, Image, Text, StatusBar
-} from 'react-native'
-import * as ImagePicker from 'expo-image-picker'
-import { Camera } from 'expo-camera'
-import Icon from 'react-native-vector-icons/MaterialIcons'
-import ExpoImageManipulator from '../manipulator/ExpoImageManipulator'
+  Dimensions, View, Image, Text, StatusBar
+} from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import { Camera } from 'expo-camera';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import ExpoImageManipulator from '../manipulator/ExpoImageManipulator';
 
-import HybridTouch from './HybridTouch'
+import HybridTouch from './HybridTouch';
 
-const noImage = require('./assets/no_image.png')
+const noImage = require('./assets/no_image.png');
 
 export default class App extends React.Component {
-    state = {
-        isVisible: false,
-        uri: null,
+  state = {
+    isVisible: false,
+    uri: null,
+  };
+  onToggleModal = () => {
+    const { isVisible } = this.state;
+    this.setState({ isVisible: !isVisible });
+  };
+  _pickImage = async () => {
+    this.setState({ uri: null });
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (status === 'granted') {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.cancelled) {
+        this.setState({
+          uri: result.uri,
+        }, () => this.setState({ isVisible: true }));
+      }
     }
-    onToggleModal = () => {
-        const { isVisible } = this.state
-        this.setState({ isVisible: !isVisible })
+  };
+
+  _pickCameraImage = async () => {
+    this.setState({ uri: null });
+    const { status } = await Camera.requestCameraPermissionsAsync();
+    if (status === 'granted') {
+      const result = await ImagePicker.launchCameraAsync();
+
+      if (!result.cancelled) {
+        this.setState({
+          uri: result.uri,
+        }, () => this.setState({ isVisible: true }));
+      }
     }
-    _pickImage = async () => {
-        this.setState({uri: null})
-        const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync()
-        if (status === 'granted') {
-            const result = await ImagePicker.launchImageLibraryAsync()
-            if (!result.cancelled) {
-                this.setState({
-                    uri: result.uri,
-                }, () => this.setState({ isVisible: true }))
-            }
-        }
-    };
+  };
 
-    _pickCameraImage = async () => {
-        this.setState({uri: null})
-        const { status } = await Camera.requestCameraPermissionsAsync()
-        if (status === 'granted') {
-            const result = await ImagePicker.launchCameraAsync()
-
-            if (!result.cancelled) {
-                this.setState({
-                    uri: result.uri,
-                }, () => this.setState({ isVisible: true }))
-            }
-        }
-    };
-
-    render() {
-        const {
-            uri, isVisible,
-        } = this.state
-        const { width, height } = Dimensions.get('window')
-        return (
-            <View style={{
-                backgroundColor: '#fcfcfc',
-                justifyContent: 'center',
-                alignContent: 'center',
-                alignItems: 'center',
-                height,
+  render() {
+    const {
+      uri, isVisible,
+    } = this.state;
+    const { width, height } = Dimensions.get('window');
+    return (
+      <View style={{
+        backgroundColor: '#fcfcfc',
+        justifyContent: 'center',
+        alignContent: 'center',
+        alignItems: 'center',
+        height,
+      }}
+      >
+        <StatusBar barStyle="dark-content" />
+        {uri ? (
+          <Image resizeMode="contain"
+            style={{
+              width, height, marginBottom: 40, backgroundColor: '#fcfcfc',
             }}
-            >
-                <StatusBar barStyle="dark-content" />
-                {uri ? (
-                    <Image resizeMode="contain"
-                        style={{
-                            width, height, marginBottom: 40, backgroundColor: '#fcfcfc',
-                        }}
-                        source={{ uri }}
-                    />
-                ) : <Image source={noImage} />}
-                <View style={{
-                    flexDirection: 'row',
-                    justifyContent: 'space-around',
-                    backgroundColor: '#2c98fd',
-                    width,
-                    position: 'absolute',
-                    bottom: 0,
-                    padding: 20,
-                }}
-                >
-                    <HybridTouch style={{ flex: 1, alignItems: 'center' }} onPress={() => this._pickImage()}>
-                        <View style={{ alignItems: 'center' }}>
-                            <Icon size={30} name="photo" color="white" />
-                            <Text style={{ color: 'white', fontSize: 18 }}>Galery</Text>
-                        </View>
-                    </HybridTouch>
-                    <HybridTouch style={{ flex: 1, alignItems: 'center' }} onPress={() => this._pickCameraImage()}>
-                        <View style={{ alignItems: 'center' }}>
-                            <Icon size={30} name="photo-camera" color="white" />
-                            <Text style={{ color: 'white', fontSize: 18 }}>Camera</Text>
-                        </View>
-                    </HybridTouch>
-                </View>
-                {
-                    uri
-                && (
-                    <ExpoImageManipulator
-                        photo={{ uri }}
-                        isVisible={isVisible}
-                        onPictureChoosed={(data) => {
-                            // console.log(data)
-                            this.setState({ uri: data.uri })
-                        }}
-                        // fixedMask={{ width: 200, height: 200 }}
-                        onToggleModal={this.onToggleModal}
-                        saveOptions={{
-                            compress: 1,
-                            format: 'png',
-                            base64: true,
-                        }}
-                        btnTexts={{
-                            done: '決定',
-                            crop: '切り抜く',
-                            processing: '処理中',
-                        }}
-                        ratio={{width: 16, height: 9}}
-                    />
-                )
-                }
+            source={{ uri }}
+          />
+        ) : <Image source={noImage} />}
+        <View style={{
+          flexDirection: 'row',
+          justifyContent: 'space-around',
+          backgroundColor: '#2c98fd',
+          width,
+          position: 'absolute',
+          bottom: 0,
+          padding: 20,
+        }}
+        >
+          <HybridTouch style={{ flex: 1, alignItems: 'center' }} onPress={() => this._pickImage()}>
+            <View style={{ alignItems: 'center' }}>
+              <Icon size={30} name="photo" color="white" />
+              <Text style={{ color: 'white', fontSize: 18 }}>Galery</Text>
             </View>
-        )
-    }
+          </HybridTouch>
+          <HybridTouch style={{ flex: 1, alignItems: 'center' }} onPress={() => this._pickCameraImage()}>
+            <View style={{ alignItems: 'center' }}>
+              <Icon size={30} name="photo-camera" color="white" />
+              <Text style={{ color: 'white', fontSize: 18 }}>Camera</Text>
+            </View>
+          </HybridTouch>
+        </View>
+        {
+          uri
+          && (
+            <ExpoImageManipulator
+              photo={{ uri }}
+              isVisible={isVisible}
+              onPictureChoosed={(data) => {
+                // console.log(data)
+                this.setState({ uri: data.uri });
+              }}
+              // fixedMask={{ width: 200, height: 200 }}
+              onToggleModal={this.onToggleModal}
+              saveOptions={{
+                compress: 1,
+                format: 'png',
+                base64: true,
+              }}
+              btnTexts={{
+                done: '決定',
+                crop: '切り抜く',
+                processing: '処理中',
+              }}
+              ratio={{ width: 16, height: 9 }}
+            />
+          )
+        }
+      </View>
+    );
+  }
 }
